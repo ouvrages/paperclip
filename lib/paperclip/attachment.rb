073e890d87edbd4ebd59fb3a802287d481cffcdf
Stephen H. Gerstacker <stephen@shortround.net>
2011-05-26 16:34:48 -0400

    Added URI escaping for the attachment URL

--- lib/paperclip/attachment.rb
+++ lib/paperclip/attachment.rb
@@ -129,7 +129,8 @@ module Paperclip
     def url(style_name = default_style, use_timestamp = @use_timestamp)
       default_url = @default_url.is_a?(Proc) ? @default_url.call(self) : @default_url
       url = original_filename.nil? ? interpolate(default_url, style_name) : interpolate(@url, style_name)
-      use_timestamp && updated_at ? [url, updated_at].compact.join(url.include?("?") ? "&" : "?") : url
+      escaped = URI.escape url
+      use_timestamp && updated_at ? [escaped, updated_at].compact.join(escaped.include?("?") ? "&" : "?") : escaped
     end
 
     # Returns the path of the attachment as defined by the :path option. If the
